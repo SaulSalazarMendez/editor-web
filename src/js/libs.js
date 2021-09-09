f2bd4a47c0375413caf8cd803b6149d84158ede3
Saúl <saul.salazar.mendez@gmail.com>
2021-09-08 23:59:29 -0500

    UPDATE - versiones de librerias

--- src/js/libs.js
+++ src/js/libs.js
@@ -4,7 +4,7 @@ export * from "codejar/linenumbers.js";
 export * from "operaciones-archivos";
 
 
-// export * from "https://unpkg.com/notificaciones-w3css@1.0.0/notificaciones.js";
+// export * from "https://unpkg.com/notificaciones-w3css@1.0.1/notificaciones.js";
 // export * from "https://unpkg.com/codejar@3.5.0/codejar.js";
 // export * from "https://unpkg.com/codejar@3.5.0/linenumbers.js";
-// export * from "https://unpkg.com/operaciones-archivos@1.0.0/operaciones-archivos.js";
+// export * from "https://unpkg.com/operaciones-archivos@1.0.2/operaciones-archivos.js";
